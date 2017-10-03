8342d85e28ade668c2239bf73b8e518fc77777a0
Ajay David <antonio.g.david@gmail.com>
2017-10-03 13:16:10 -0400

    Use Components with children

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,18 @@
 // Code ThemedDecoration Component Here
+
+import React, { Component, Children, cloneElement } from 'react';
+
+export default class ThemedDecorations extends Component {
+  render() {
+    const childrenWithExtraProp = Children.map(this.props.children, child => cloneElement(child, {
+      className: this.props.theme
+      })
+    );
+
+    return (
+        <div>
+          {childrenWithExtraProp}
+        </div>
+    );
+  }
+}
